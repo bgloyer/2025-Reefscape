1fad27946e4501190afebf93e7f1e29a2f3420a6
Aidan-Kelley~ <aidank6@yahoo.com>
2025-02-12 00:27:03 -0800

    warm up path planner path following

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,9 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.Helpers;
+
+import com.pathplanner.lib.commands.FollowPathCommand;
+
 import au.grapplerobotics.CanBridge;
 
 /**
@@ -40,6 +43,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    FollowPathCommand.warmupCommand().schedule(); // warm up path planner path following
   }
 
   /**
