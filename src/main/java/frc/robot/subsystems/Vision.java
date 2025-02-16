22e44b1ae04bd1efd3118b18af3ec25394a91dc0
John Doe <aidank6@yahoo.com>
2025-02-15 16:12:15 -0800

    don't use vision pose if too far away

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.constants.VisionConstants;
 import frc.robot.util.LimelightHelpers;
 
@@ -22,8 +23,8 @@ public class Vision {
 
     private void addToPoseEstimator(String limelightName, Pigeon2 gyro) {
         boolean doRejectUpdate = false;
-        LimelightHelpers.SetRobotOrientation(VisionConstants.ReefLightLightName, m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
-        LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(VisionConstants.ReefLightLightName);
+        LimelightHelpers.SetRobotOrientation(limelightName, m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+        LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
         if(mt2 == null)
             return;
         if (Math.abs(gyro.getRate()) > 720)  { // if our angular velocity is greater than 720 degrees per second, ignore vision updates
@@ -32,9 +33,15 @@ public class Vision {
         if (mt2.tagCount == 0) {
             doRejectUpdate = true;
         }
+        if (mt2.avgTagDist > 1.5) {
+            doRejectUpdate = true;
+        }
         if (!doRejectUpdate) {
             // stolen from 1678
-            double xyStdDev = (0.1) * ((0.01 * Math.pow(LimelightHelpers.getRawFiducials(limelightName)[0].distToCamera, 2.0)) + (0.005 * Math.pow(mt2.avgTagDist, 2.0))) / mt2.tagCount;
+            double xyStdDev = 2 * ((0.01 + (0.005 * Math.pow(mt2.avgTagDist, 2.0))) / mt2.tagCount);
+
+            SmartDashboard.putNumber("mt2 avg dist", mt2.avgTagDist);
+            SmartDashboard.putNumber("xyStdDev", xyStdDev);
             xyStdDev = Math.max(0.02, xyStdDev);
                         m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(xyStdDev, xyStdDev, 9999999));
                         m_poseEstimator.addVisionMeasurement(
