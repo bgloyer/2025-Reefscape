8579581f4170381b835cb511586b08459bc0c156
John Doe <aidank6@yahoo.com>
2025-02-20 13:20:19 -0800

    little algae intake tuning

--- src/main/java/frc/robot/constants/AlgaeIntakeConstants.java
+++ src/main/java/frc/robot/constants/AlgaeIntakeConstants.java
@@ -4,12 +4,12 @@ public class AlgaeIntakeConstants {
 
     public static final int PivotMotorId = 20;
     public static final int RollerMotorId = 21;
-    public static final double kP = 0.001;
+    public static final double kP = 0.01;
     public static final double kI = 0;
-    public static final double kD = 0.01;
+    public static final double kD = 0.5;
     public static final double StoreAngle = 0;
     public static final double IntakeAngle = 60;
-    public static final double ClimbAngle = 110;
+    public static final double ClimbAngle = 109;
     public static final double GearRatio = 18.0 / 60.0;
     public static final double IntakeVoltage = 3;
 
